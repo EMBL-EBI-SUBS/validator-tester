82e4c341f634e01e62ab57935af1d631e809afb2
Flavia Penim <flaviapenim@gmail.com>
2017-05-10 15:27:28 +0100

    Initial project structure.

---
+++ src/main/java/uk/ac/ebi/subs/validator/tester/TestingApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.subs.validator.tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingApplication.class, args);
+    }
+}
