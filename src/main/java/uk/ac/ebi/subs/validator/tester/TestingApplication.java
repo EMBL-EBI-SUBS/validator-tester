9832fba798e3ac78a487b70ac82f4c08ad446897
Karoly Erdos <karoly@ebi.ac.uk>
2017-05-12 15:45:05 +0100

    Add a simple message publisher for testing the validation prototype

--- src/main/java/uk/ac/ebi/subs/validator/tester/TestingApplication.java
+++ src/main/java/uk/ac/ebi/subs/validator/tester/TestingApplication.java
@@ -1,12 +1,25 @@
 package uk.ac.ebi.subs.validator.tester;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import uk.ac.ebi.subs.processing.SubmissionEnvelope;
+import uk.ac.ebi.subs.validator.tester.submissions.SubmissionPublisher;
 
 @SpringBootApplication
-public class TestingApplication {
+public class TestingApplication implements CommandLineRunner  {
+
+    @Autowired
+    SubmissionPublisher publisher;
 
     public static void main(String[] args) {
         SpringApplication.run(TestingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        SubmissionEnvelope submissionEnvelope = publisher.createASubmissionToPublish();
+        publisher.publishASubmisssionEnvelope(submissionEnvelope);
     }
 }
